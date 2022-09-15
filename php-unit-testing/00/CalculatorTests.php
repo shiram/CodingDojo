23dd1e344143d671a59342f969829495e2889561
shiram <shirambereh@gmail.com>
2022-09-15 22:52:49 +0000

    double test - stub example

--- php-unit-testing/00/CalculatorTests.php
+++ php-unit-testing/00/CalculatorTests.php
@@ -1,5 +1,28 @@
 <?php
 
+/**
+ * 
+ *      
+#When to use test double
+
+As mentioned in the first part of this series. One of PHPUnit's powerful features is test double. It is very common in our code, a function of one class is calling another class's function. In this case, we have a dependency in these two classes. In particular, the caller class has a dependency on calling class. But as we already know in part 1, unit test should test the smallest unit of functionality, in this case, it should test only the caller function. To solve this problem, We can use test double to replace the calling class. Since a test double can be configured to return predefined results, we can focus on testing the caller function.
+#Types of test doubles
+
+Test double is a generic term for objects we use, to replace real production ready objects. In our opinion, it is very useful to categorize test doubles by their purpose. It does not only make it easy for us to understand the test case, but also make our code friendly to other parties.
+
+Accordingly to Martin Fowler's post, There are five types of test double:
+
+    Dummy objects are passed around but never actually used. Usually they are just used to fill parameter lists.
+    Fake objects actually have working implementations, but usually take some shortcut which makes them not suitable for production.
+    Stubs provide canned answers to calls made during the test, usually not responding at all to anything outside what's programmed in for the test.
+    Spies are stubs that also record some information based on how they were called. One form of this might be an email service that records how many messages it was sent.
+    Mocks are pre-programmed with expectations which form a specification of the calls they are expected to receive. They can throw an exception if they receive a call they don't expect and are checked during verification to ensure they got all the calls they were expecting.
+
+#How to create test double
+
+PHPUnit's method getMockBuilder can be used to create any similar user defined objects. Combining with its configurable interface. We can use it to create basically all five types of test doubles.
+ */
+
 require 'Calculator.php';
 
 class CalculatorTests extends \PHPUnit\Framework\TestCase
@@ -76,5 +99,22 @@ class CalculatorTests extends \PHPUnit\Framework\TestCase
     public function testMultiply($a, $b, $expected) {
         $result = $this->calculator->multiply($a, $b);
         $this->assertEquals($expected, $result);
+    }
+
+
+    /**
+     *  @dataProvider addDataProvider
+     */
+    public function testWithStub($a, $b, $expected) {
+        //create stub for Calculator class
+        $calculator = $this->getMockBuilder('Calculator')
+                            ->getMock();
+
+        //configure the stub
+        $calculator->expects($this->any())
+                    ->method('add')
+                    ->will($this->returnValue($expected));
+
+        $this->assertEquals($expected, $calculator->add($a, $b));
     }
 }
