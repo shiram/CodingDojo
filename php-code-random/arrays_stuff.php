ba34811699e50707aeb6fc0b3a4e8ef4cbd926b1
User <anon@localhost.localdomain>
2022-09-20 15:25:28 -0400

    array stuff using explode

--- php-code-random/arrays_stuff.php
+++ php-code-random/arrays_stuff.php
@@ -5,7 +5,16 @@
  */
 
  function stringToAssociativeArray(array $data) {
-
+    $newArray = [];
+    for ($i = 0; $i < sizeof($data); $i++) {
+       $token = explode('.', $data[$i]);
+       if (array_key_exists($token[0], $newArray)) {
+          array_push($newArray[$token[0]], $token[1]);
+       } else {
+          $newArray[$token[0]] = array($token[1]);
+       }
+    }
+    return $newArray;
  }
 
  $data = ["claims" => [
@@ -17,4 +26,8 @@
  ]
 ];
 
-var_dump($data['claims']);
+// var_dump($data['claims']);
+
+$res = stringToAssociativeArray($data['claims']);
+
+var_dump($res);
