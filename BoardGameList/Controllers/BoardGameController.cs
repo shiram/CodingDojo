d21f0aa64697d0834606539c4700393b5fcaf165
Shirambere Edward <EShirambere@dfcugroup.com>
2024-02-10 22:53:13 +0300

    adding a model and controller to simulate with swagger

---
+++ BoardGameList/Controllers/BoardGameController.cs
@@ -0,0 +1,49 @@
+﻿using BoardGameList.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BoardGameList.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BoardGameController : ControllerBase
+    {
+        private ILogger<BoardGameController> _logger;
+
+        public BoardGameController(ILogger<BoardGameController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet(Name = "GetBoardGames")]
+        public IEnumerable<BoardGame> Get()
+        {
+            return new[] {
+                new BoardGame {
+                    Id = 1,
+                    Name = "Edward",
+                    Year = 1992
+                },
+                new BoardGame {
+                    Id = 2,
+                    Name = "Sam",
+                    Year = 1996
+                },
+                new BoardGame {
+                    Id = 3,
+                    Name = "Tim",
+                    Year = 1997
+                },
+                new BoardGame {
+                    Id = 4,
+                    Name = "Tevin",
+                    Year = 2005
+                },
+                new BoardGame {
+                    Id = 5,
+                    Name = "Riley",
+                    Year = 2022
+                }
+            };
+        }
+    }
+}
