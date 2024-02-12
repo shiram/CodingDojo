2d5556b3d7483de92a4ff31047376d3638a8640b
Shirambere Edward <shirambereh@gmail.com>
2024-02-13 01:31:35 +0300

    add static files and templates

--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 #### The Things I have Learnt
 + Setup a flask app
 + Setup configuration using class based configs and set a config using app.config.from_object
++ Add static files, add templates and display image from static folder
--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 from config import *
 
 app = Flask(__name__)
@@ -7,6 +7,10 @@ app.config.from_object(DevelopmentConfig)
 @app.route('/')
 def hello_world():
     return "hello from flask"
+
+@app.route('/test-view')
+def test_view():
+    return render_template('test.html')
 
 if __name__ == '__main__':
     app.run()
