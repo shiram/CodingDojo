247f207d6bb5535a844d2ef9eb9bad348717dc6e
Shirambere Edward <EShirambere@dfcugroup.com>
2025-06-19 12:52:42 +0300

    remove login logic from main app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,68 +1,12 @@
-import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
-import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, CommonModule, FormsModule],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'Homes';
-  email: string = '';
-  password: string = '';
-  showPassword: boolean = false;
-
-  constructor(private router: Router) {}
-
-  togglePassword(): void {
-    this.showPassword = !this.showPassword;
-  }
-
-  onSubmit(): void {
-    if(this.email && this.password) {
-      console.log('Email:', this.email);
-      console.log('Password: ', this.password);
-    }
-  }
-
-  goToSignUpPage(): void {
-    console.log("Navigating to Sign Up page");
-    // Here you would typically use a router to navigate to the sign-up page
-    // For example: this.router.navigate(['/register']);
-    this.router.navigate(['/register']);
-  }
-
-  signInWithGoogle(): void {
-    console.log("Sign in with Google clicked");
-    // Here you would typically call a service to handle Google sign-in
-  }
-
-  signInWithFacebook(): void {
-    console.log("Sign in with Facebook clicked");
-    // Here you would typically call a service to handle Facebook sign-in
-  }
-
-  signInWithTwitter(): void {
-    console.log("Sign in with Twitter clicked");
-    // Here you would typically call a service to handle Twitter sign-in
-  }
-
-  signInWithLinkedIn(): void {
-    console.log("Sign in with LinkedIn clicked");
-    // Here you would typically call a service to handle LinkedIn sign-in
-  }
-
-  signInWithGitHub(): void {
-    console.log("Sign in with GitHub clicked");
-    // Here you would typically call a service to handle GitHub sign-in
-  }
-
-  signInWithApple(): void {
-    console.log("Sign in with Apple clicked");
-    // Here you would typically call a service to handle Apple sign-in
-  }
 }
