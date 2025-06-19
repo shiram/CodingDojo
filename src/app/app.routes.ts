65bc14da18a4959abea022fd037d9601dd9a4efb
Shirambere Edward <EShirambere@dfcugroup.com>
2025-06-19 12:53:32 +0300

    add new routes login and register

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,14 +1,12 @@
 import { Routes } from '@angular/router';
-import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 
 export const routes: Routes = [
     {
-        path: '', component: AppComponent, 
-        title: 'Home',
+        path: '', component: LoginComponent
     },
     {
-        path: 'register', component: RegisterComponent, 
-        title: 'Register',
+        path: 'register', component: RegisterComponent
     }
 ];
